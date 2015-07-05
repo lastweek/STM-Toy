021160b84e8da0d531a6fd1cbab4dce9f2c5c5be
Shan Yizhou <553997109@qq.com>
2015-07-05 14:25:27 +0800

    up

--- stm.c
+++ stm.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <setjmp.h>
 
-/* FIXME Tentative Global... */
+/* FIXME Tentative Global...
+ * For now, assume that one thread only
+ * have one struct transaction instance */
 struct transaction trans;
 
 /* Contention Policy */
@@ -20,33 +21,44 @@ enum TM_STATUS {
 	TM_ABORT	= 3
 };
 
+/* Read Data Entry in Read-Set */
 struct r_entry {
 
 };
+
+/* Write Data Entry in Write-Set */
 struct w_entry {
 
 };
 
-/* Read Data Set in Transaction */
-struct r_set {
+/* Read-Set in Transaction */
+struct read_set {
 	struct r_entry *rlist;
 	int nr_entries;
 };
 
-/* Write Data Set in Transaction */
-struct w_set {
+/* Write-Set in Transaction */
+struct write_set {
 	struct w_entry *wlist;
 	int nr_entries;
 };
 
 /* Transaction Descriptor */
 struct transaction {
-	volatile int status;
 	jmp_buf jb;
-	struct r_set rs;
-	struct w_set ws;
+	volatile int status;
+	struct read_set  rs;
+	struct write_set ws;
 	int start_time;
 	int end_time;
+};
+
+/* Ownership record for every TM byte */
+struct ownership_record {
+	struct transaction *owner;
+	int version;
+	char old;
+	char new;
 };
 
 static void tm_set_status(struct transaction *t, int status)
@@ -75,7 +87,12 @@ void tm_start(struct transaction *t)
  */
 int tm_commit(void)
 {
+	int status;
+	status = tm_read_status(&trans);
 	
+	tm_set_status(&trans, TM_COMMITED);
+	
+	return (status == TM_ABORT);
 }
 
 /**
@@ -84,7 +101,8 @@ int tm_commit(void)
  */
 int tm_validate(void)
 {
-	int status = tm_read_status(&trans);
+	int status;
+	status = tm_read_status(&trans);
 	return (status == TM_ACTIVE);
 }
 
@@ -100,6 +118,9 @@ void tm_abort(void)
  */
 char tm_read_addr(void *addr)
 {
+	/* Map to orec */
+
+
 	return *(char *)addr;
 }
 
@@ -112,9 +133,23 @@ void tm_write_addr(void *addr)
 
 }
 
+
+
 /* General Purpose STM Programming API */
-#define __TM_START__	\
-	tm_start(&trans); setjmp(trans.jb);
+#define __TM_START__				\
+	{								\
+		tm_start(&trans);			\
+		setjmp(trans.jb);			\
+	}
+
+#define __TM_END__					\
+	{								\
+		if (tm_commit()) {			\
+			/* Start again */		\
+			longjmp(trans.jb, 1);	\
+		}							\
+		/* set end time */			\
+	}
 
 #define TM_ABORT()		tm_abort()
 #define TM_COMMIT()		tm_commit()
@@ -126,11 +161,13 @@ void tm_write_addr(void *addr)
 int main(void)
 {
 	char share = 'A';
-	__TM_START__ {
-		TM_READ_ADDR(&share);
-	}
+	int t=1;
 
-	printf("%c\n", share);
+	__TM_START__ {
+		
+	
+	}__TM_END__
+
 	return 0;
 }
 
