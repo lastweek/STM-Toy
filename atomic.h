10f57c291aece297240611fcecc2a7290b330511
Shan Yizhou <553997109@qq.com>
2015-07-08 11:51:33 +0800

    up

--- atomic.h
+++ atomic.h
@@ -11,6 +11,10 @@
  */
 
 typedef int atomic_t;
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned int		u32;
+typedef unsigned long long	u64;
 
 static inline char atomic_read_char(atomic_t *p)
 {
@@ -48,13 +52,51 @@ static inline void atomic_or(atomic_t *p, int mask)
  * Compare @OLD with @MEM, if identical, store @NEW in memory.
  * Success is indicated by comparing @RETURN with @OLD
  */
+#define lock "lock; "
 #define atomic_cmpxchg(ptr, old, new)  __cmpxchg(ptr, old, new, sizeof(*(ptr)))
-
-#define __cmpxchg(ptr, old, new, size)	\
-({	\
-	__typeof__(*(ptr)) __ret;	\
-	__ret;	\
+#define __cmpxchg(ptr, old, new, size)					\
+({														\
+	__typeof__(*(ptr)) __ret;							\
+	__typeof__(*(ptr)) __old = (old);					\
+	__typeof__(*(ptr)) __new = (new);					\
+	switch (size) {										\
+	case 1:												\
+	{													\
+		volatile u8 *__ptr = (volatile u8 *)(ptr);		\
+		asm volatile(lock "cmpxchgb %2,%1"				\
+			     : "=a" (__ret), "+m" (*__ptr)			\
+			     : "q" (__new), "0" (__old)				\
+			     : "memory");							\
+		break;											\
+	}													\
+	case 2:												\
+	{													\
+		volatile u16 *__ptr = (volatile u16 *)(ptr);	\
+		asm volatile(lock "cmpxchgw %2,%1"				\
+			     : "=a" (__ret), "+m" (*__ptr)			\
+			     : "r" (__new), "0" (__old)				\
+			     : "memory");							\
+		break;											\
+	}													\
+	case 4:												\
+	{													\
+		volatile u32 *__ptr = (volatile u32 *)(ptr);	\
+		asm volatile(lock "cmpxchgl %2,%1"				\
+			     : "=a" (__ret), "+m" (*__ptr)			\
+			     : "r" (__new), "0" (__old)				\
+			     : "memory");							\
+		break;											\
+	}													\
+	case 8:												\
+	{													\
+		volatile u64 *__ptr = (volatile u64 *)(ptr);	\
+		asm volatile(lock "cmpxchgq %2,%1"				\
+			     : "=a" (__ret), "+m" (*__ptr)			\
+			     : "r" (__new), "0" (__old)				\
+			     : "memory");							\
+		break;											\
+	}													\
+	}													\
+	__ret;												\
 })
-
-
 
